3826e833f2efa2901c0f2d77cd28ccb1fe4865a0
AndreNicacio <nicaciodagga@gmail.com>
2023-09-17 16:00:18 -0300

    Ecossistema AWS para gerenciamento das funções da Heal Tech Udna

---
+++ amplify/#current-cloud-backend/function/getExams/src/index.js
@@ -0,0 +1,32 @@
+/* Amplify Params - DO NOT EDIT
+You can access the following resource attributes as environment variables from your Lambda function
+var environment = process.env.ENV
+var region = process.env.REGION
+var authUdnabackendbe02c040UserPoolId = process.env.AUTH_UDNABACKENDBE02C040_USERPOOLID
+var apiUdnaBackendGraphQLAPIIdOutput = process.env.API_UDNABACKEND_GRAPHQLAPIIDOUTPUT
+var apiUdnaBackendGraphQLAPIEndpointOutput = process.env.API_UDNABACKEND_GRAPHQLAPIENDPOINTOUTPUT
+
+Amplify Params - DO NOT EDIT */
+
+const { pagarmeAPI, PAGARME_API_KEY_DEV, PAGARME_API_KEY_PRD } = require('./pagarmeClient');
+
+exports.handler = async (event) => {
+  const PAGARME_API_KEY = process.env.ENV === 'prd' ? PAGARME_API_KEY_DEV : PAGARME_API_KEY_PRD;
+  try {
+    const res = await pagarmeAPI.get(`/transactions?count=100&customer[external_id]=${event.arguments.username}&api_key=${PAGARME_API_KEY}`);
+    console.log(res.data);
+    return res.data.map((elt) => ({
+      id: elt.id,
+      examId: elt.items[0].id,
+      name: elt.items[0].title,
+      price: elt.items[0].unit_price,
+      payment: elt.payment_method,
+      installments: Number(elt.installments),
+      status: elt.status,
+      date: elt.date_created,
+    }));
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
